fe934cec0b7788e628b5bea1a5bf6d22f2e5e645
Chmod351 <yamil.t.351@proton.me>
2024-08-04 19:11:45 -0300

    fix idchecker

--- src/application/orders/orderRoutes.ts
+++ src/application/orders/orderRoutes.ts
@@ -5,6 +5,8 @@ import orderController from './orderControllers';
 const routes = Router();
 
 routes.get('/', orderController.findAll);
-routes.put('/update/:id', idChecker, orderController.updateOrderStatus);
+routes.put('/update/:id', idChecker.containsIdInParams, orderController.updateOrderStatus);
 routes.post('/create', orderController.createOrder);
-routes.delete('/delete/:id', idChecker, orderController.deleteOrder);
+routes.delete('/delete/:id', idChecker.containsIdInParams, orderController.deleteOrder);
+
+export default routes;
