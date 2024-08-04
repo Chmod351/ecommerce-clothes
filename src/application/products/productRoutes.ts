95074a045e9d18fa2f93d480ec9086a62d3eccd1
Chmod351 <yamil.t.351@proton.me>
2024-08-04 18:26:56 -0300

    linter

--- src/application/products/productRoutes.ts
+++ src/application/products/productRoutes.ts
@@ -5,9 +5,9 @@ const routes = Router();
 
 routes.get('/', productsController.findAll);
 routes.get('/search', productsController.findByQuery);
-routes.get('/:id', idChecker, productsController.findById);
-routes.post('/create', productsController.create);
-routes.put('/update/:id', idChecker, productsController.update);
-routes.delete('/delete/:id', idChecker, productsController.delete);
+routes.get('/:id', idChecker.containsIdInParams, productsController.findById);
+routes.post('/create', idChecker.isUserAdmin, productsController.create);
+routes.put('/update/:id', idChecker.containsIdInParams, productsController.update);
+routes.delete('/delete/:id', idChecker.containsIdInParams, productsController.delete);
 
 export default routes;
