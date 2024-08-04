2fdf1698513ac050ae2eec0312aaafe7e067f3f3
Chmod351 <yamil.t.351@proton.me>
2024-08-04 18:26:19 -0300

    add async and add monthly report

--- src/application/orders/orderControllers.ts
+++ src/application/orders/orderControllers.ts
@@ -9,6 +9,7 @@ class OrderController {
       const orders: IOrder[] = await orderService.findAll(page);
       res.status(200).json(orders);
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }
@@ -19,25 +20,70 @@ class OrderController {
       const updatedOrder = await orderService.updateOrderStatus(id, status);
       res.status(200).json(updatedOrder);
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }
 
-  createOrder(req: Request, res: Response, next: NextFunction) {
+  async createOrder(req: Request, res: Response, next: NextFunction) {
     const { body } = req;
     try {
       const createdOrder = orderService.createOrder(body);
       res.status(200).json(createdOrder);
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }
-  deleteOrder(req: Request, res: Response, next: NextFunction) {
+  async deleteOrder(req: Request, res: Response, next: NextFunction) {
     const { id } = req.params;
     try {
       const deletedOrder = orderService.deleteOrder(id);
       res.status(200).json(deletedOrder);
     } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+  // sell metrics here
+
+  async getMonthlySalesReport(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { month, year } = req.query;
+      const startOfMonth = new Date(`${year}-${month}-01`);
+      const endOfMonth = new Date(startOfMonth);
+      endOfMonth.setMonth(endOfMonth.getMonth() + 1);
+      endOfMonth.setDate(1);
+
+      const pipeline = [
+        {
+          $match: {
+            createdAt: {
+              $gte: startOfMonth,
+              $lt: endOfMonth,
+            },
+          },
+        },
+        {
+          $group: {
+            _id: { $month: '$createdAt' },
+            city: { $first: '$shippingAddress.city' },
+            country: { $first: '$shippingAddress.country' },
+            date: { $first: '$createdAt' },
+            sucessfulOrders: { $sum: { $cond: [{ $eq: ['$status', 'Delivered'] }, 1, 0] } },
+            totalOrders: { $sum: 1 },
+            totalProducts: { $sum: '$products.quantity' },
+            totalSales: { $sum: '$totalPrice' },
+          },
+        },
+        {
+          $sort: { month: 1, year: 1 },
+        },
+      ];
+      const salesReport = await orderService.getMonthlySalesReport(pipeline, 1);
+      res.status(200).json(salesReport);
+    } catch (error) {
+      console.log(error);
       next(error);
     }
   }
