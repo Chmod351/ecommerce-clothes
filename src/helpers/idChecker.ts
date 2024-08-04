98322dc60620c5cf8a6308ffb2e7180b2f49fbc0
Chmod351 <yamil.t.351@proton.me>
2024-08-04 19:21:05 -0300

    add admin validation

--- src/helpers/idChecker.ts
+++ src/helpers/idChecker.ts
@@ -1,13 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
+import { MySessionData } from '../application/users/userTypes';
 import mongoose from 'mongoose';
 
-function idChecker(req: Request, res: Response, next: NextFunction) {
+function containsIdInParams(req: Request, res: Response, next: NextFunction) {
   const id: string = req.params._id;
   if (!id) {
     return next(new Error('Missing id parameter'));
   } else if (!mongoose.Types.ObjectId.isValid(id)) {
+    console.log(`Invalid id: ${id}`);
     return next(new Error('Invalid ID'));
   }
   next();
 }
+function isUserAdmin(req: Request, res: Response, next: NextFunction) {
+  const userType = (req.session as MySessionData).user?.type;
+  if (!req.session || userType !== 'admin') {
+    return res.status(401).json({ error: 'Unauthorized' });
+  } else {
+    next();
+  }
+}
+const idChecker = {
+  containsIdInParams,
+  isUserAdmin,
+};
 export default idChecker;
