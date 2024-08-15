7fd54e220b65f6846f4f4cb2ddd31352242f039b
Chmod351 <yamil.t.351@proton.me>
2024-08-14 22:49:34 -0300

    add is admin helpers to orders routes

--- src/application/orders/orderRoutes.ts
+++ src/application/orders/orderRoutes.ts
@@ -4,9 +4,9 @@ import orderController from './orderControllers';
 
 const routes = Router();
 
-routes.get('/', orderController.findAll);
-routes.put('/update/:id', idChecker.containsIdInParams, orderController.updateOrderStatus);
+routes.get('/', idChecker.isUserAdmin, orderController.findAll);
+routes.put('/update/:id', idChecker.containsIdInParams, idChecker.isUserAdmin, orderController.updateOrderStatus);
 routes.post('/create', orderController.createOrder);
-routes.delete('/delete/:id', idChecker.containsIdInParams, orderController.deleteOrder);
+routes.delete('/delete/:id', idChecker.containsIdInParams, idChecker.isUserAdmin, orderController.deleteOrder);
 
 export default routes;
