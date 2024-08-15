24526567e4de40c053566c819553ad86f70e7160
Chmod351 <yamil.t.351@proton.me>
2024-08-14 22:47:52 -0300

    fix product type, image is an array, add support to size support

--- src/application/products/productTypes.ts
+++ src/application/products/productTypes.ts
@@ -6,12 +6,54 @@ export interface IProduct extends mongoose.Document {
   name_en: string;
   description_es: string;
   description_en: string;
-  image_url: string;
+  image_url: [string];
   price_es: number;
   price_en: number;
   category: string;
   seasson: string;
-  stock: number;
+  stock: [
+    {
+      color: [string];
+      provider: string;
+      providerCost: number;
+      quantity: number;
+      size: [
+        'XS',
+        'S',
+        'M',
+        'L',
+        'XL',
+        'XXL',
+        'XXXL',
+        'XXXXL',
+        '25',
+        '26',
+        '27',
+        '28',
+        '29',
+        '30',
+        '31',
+        '32',
+        '33',
+        '34',
+        '35',
+        '36',
+        '37',
+        '38',
+        '39',
+        '40',
+        '41',
+        '42',
+        '43',
+        '44',
+        '45',
+        '46',
+        '47',
+        '48',
+      ];
+    },
+  ];
+  weight: number;
   createdAt?: Date;
   updatedAt?: Date;
 }
@@ -21,10 +63,52 @@ export interface IProductBody {
   name_en: string;
   description_es: string;
   description_en: string;
-  image_url: string;
+  image_url: [string];
   price_es: number;
   price_en: number;
   category: string;
   seasson: string;
-  stock: number;
+  weight: number;
+  stock: [
+    {
+      color: [string];
+      provider: string;
+      providerCost: number;
+      quantity: number;
+      size: [
+        'XS',
+        'S',
+        'M',
+        'L',
+        'XL',
+        'XXL',
+        'XXXL',
+        'XXXXL',
+        '25',
+        '26',
+        '27',
+        '28',
+        '29',
+        '30',
+        '31',
+        '32',
+        '33',
+        '34',
+        '35',
+        '36',
+        '37',
+        '38',
+        '39',
+        '40',
+        '41',
+        '42',
+        '43',
+        '44',
+        '45',
+        '46',
+        '47',
+        '48',
+      ];
+    },
+  ];
 }
